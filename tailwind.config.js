fb792468bcc2208541ba1b8163e6a7e685d59f6b
sadjoaldi <alsannealdi@hotmail.com>
2024-05-04 03:22:34 +0200

    creation projet abstract avec tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue_gray: "#dadbf1",
+        links_color: "#4C5FD5",
+      },
+    },
+  },
+  plugins: [],
+};
